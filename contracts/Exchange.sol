03c9b048dbd842bd78ae50344f64059342b3b1d8
xuyingchu <2577863880@qq.com>
2023-04-22 00:24:51 +0800

    feat:完成exchange合约编写和测试。

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -16,8 +16,72 @@ contract Exchange{
     //币类型=》账号地址=》数量
     mapping (address=>mapping(address=>uint256)) public tokens;
 
+    //订单结构体
+    //订单结构体
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+
+        address tokenGive;
+        uint256 amountGive;
+
+        uint256 timestamp;
+    }
+
+
+    //订单 列表
+    mapping(uint256=>_Order) public orders;
+
+    //订单数量
+
+    uint256 public orderCount;
+
+
+
+    //存储订单是否取消
+    mapping(uint256=>bool) public orderCancel;
+
+    //存储订单是否交易
+    mapping(uint256=>bool) public orderFill;
+
     event Deposit(address tokenType,address user, uint value ,uint256 balance);
     event WithDraw(address tokenType,address user, uint value ,uint256 balance);
+    //创建订单事件
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    //取消订单事件
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    //填充订单
+    
+    event Trade(
+            uint256 id,
+            address user,
+            address fillUser,
+            address tokenGet,
+            uint256 amountGet,
+            address tokenGive,
+            uint256 amountGive,
+            uint256 timestamp
+        );
 
     constructor(address _feeAccount ,uint256 _feePercent){
         feeAccount=_feeAccount;
@@ -70,9 +134,76 @@ contract Exchange{
         emit  WithDraw(_token, msg.sender,_amount, tokens[_token][msg.sender]);
     }
 
-
-
+    //查余额
+    function balanceOf(address _token,address _user) public view returns (uint256) {
+        return tokens[_token][_user];
+    }
     
+    //makeOrder
+    //_tokenGive 出多少xxxToken   获取多少 xxx_token
+    function makeOrder(address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive) public 
+        {
+        require(balanceOf(_tokenGive,msg.sender)>=_amountGive,unicode"创建订单时余额不足");
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(orderCount,msg.sender,_tokenGet,_amountGet,_tokenGive,_amountGive,block.timestamp);
+        //发出订单
+        emit Order(orderCount,msg.sender,_tokenGet,_amountGet,_tokenGive,_amountGive,block.timestamp);
+    }
+
+
+    //cancel 取消订单
+    //cancelOrder
+    function cancelOrder(uint256 _id) public {
+        _Order memory myorder = orders[_id];
+        require(myorder.user==msg.sender);
+        require(myorder.id == _id);
+        orderCancel[_id] = true;
+        emit Cancel(myorder.id,msg.sender,myorder.tokenGet,myorder.amountGet,myorder.tokenGive,myorder.amountGive,block.timestamp);
+    }
+    
+
+    //填充订单
+    function fillOrder(uint256 _id)public{
+
+        _Order memory myorder = orders[_id];
+       
+        require(myorder.id == _id);
+
+        orderFill[_id] = true;
+
+        //msg.sender,
+
+        //假如有一个订单
+        //用tokenGet  换取一定数量的 tokenGive
+
+
+        //小费计算，填充订单的人支付
+        uint256 feeAmout = myorder.amountGet.mul(feePercent).div(100);
+
+
+        require(balanceOf(myorder.tokenGet, msg.sender)>=myorder.amountGet.add(feeAmout),unicode"填充订单的人余额不足");
+        require(balanceOf(myorder.tokenGive, myorder.user)>=myorder.amountGive,unicode"创建订单的人余额不足");
+
+        tokens[myorder.tokenGet][msg.sender]=tokens[myorder.tokenGet][msg.sender].sub(myorder.amountGet.add(feeAmout));
+        tokens[myorder.tokenGet][myorder.user]=tokens[myorder.tokenGet][myorder.user].add(myorder.amountGet);
+
+
+        //转账到freeAccount
+         tokens[myorder.tokenGet][feeAccount] = tokens[myorder.tokenGet][feeAccount].add(feeAmout);
+
+
+        
+        tokens[myorder.tokenGive][msg.sender]=tokens[myorder.tokenGive][msg.sender].add(myorder.amountGive);
+        tokens[myorder.tokenGive][myorder.user]=tokens[myorder.tokenGive][myorder.user].sub(myorder.amountGive);
+
+
+
+
+        emit Trade(myorder.id,myorder.user,msg.sender,myorder.tokenGet,myorder.amountGet,myorder.tokenGive,myorder.amountGive,block.timestamp);
+    }
 
 
         
